c4fbca22bd80b530542acdd7c20ae41fdb1ea90d
Rishabh-721 <rishabhrajput337@gmail.com>
2026-05-24 11:20:09 +0530

    removed process exit from line 9 that was creating issue and exiting process

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -4,9 +4,8 @@ const dotenv = require("dotenv");
 
 const connectDB = async(req,res)=>{
     try {
-    const conn = await new mongoose.connect(process.env.MONGO_URL);
+    const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log(`server is connect to database ${conn.connection.host}`);
-    process.exit(0);
     } catch (error) {
     console.error(`Database Error ${error}`);
     process.exit(1);
