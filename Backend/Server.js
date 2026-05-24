2a091452ff79ab6208536dcd63d466e5af011828
Rishabh-721 <rishabhrajput337@gmail.com>
2026-05-24 11:19:00 +0530

    connected server to database

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 dotenv.config();
 
@@ -8,6 +9,8 @@ const app = express();
 
 app.use(express.json());
 
+connectDB();
+
 app.get("/", async(req, res) => {
     await res.send(`<h1>Apis are working</h1>`);
 });
@@ -15,3 +18,4 @@ app.get("/", async(req, res) => {
 app.listen(port, res => {
     console.log(`server is running on port: ${port}`)
 })
+
