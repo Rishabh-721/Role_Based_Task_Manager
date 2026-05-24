b2a921a8530c8e980a9012cfae7009e396351c37
Rishabh-721 <rishabhrajput337@gmail.com>
2026-05-24 11:15:17 +0530

    changed user model and added password

--- Backend/model/User.model.js
+++ Backend/model/User.model.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema({
         lowercase: true,
         unique: true,
     },
-    passowrd:{
+    password:{
         type: String,
         required: true,
     },
