f13fc2cdac62b39e5cf188e47d50126048f46bf4
Rishabh-721 <rishabhrajput337@gmail.com>
2026-05-24 12:17:14 +0530

    server is connected to auth routes

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const authRoutes = require("./router/auth.router");
 
 dotenv.config();
 
@@ -12,8 +13,10 @@ app.use(express.json());
 connectDB();
 
 app.get("/", async(req, res) => {
-    await res.send(`<h1>Apis are working</h1>`);
+    await res.send(`<h1> apis are working on port: ${port} and on database : ${process.env.MONGO_URL}</h1>`);
 });
+
+app.use("/user/auth", authRoutes);
 
 app.listen(port, res => {
     console.log(`server is running on port: ${port}`)
