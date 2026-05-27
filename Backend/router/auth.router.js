278cbde47f94a74b2ac080939260ced7ff7738d3
Rishabh-721 <rishabhrajput337@gmail.com>
2026-05-27 12:20:29 +0530

    added proper route for each so that everyuser can be verified and change their password without verification

--- Backend/router/auth.router.js
+++ Backend/router/auth.router.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const userModel = require("../model/user.model");
 const routes = express.Router();
-const { signup, login, getMe } = require("../controller/auth.controller")
+const { signup, login, getMe, forgotPassword, verifyMailing, userVerification, changePassword } = require("../controller/auth.controller")
 const auth = require("../middleware/auth.middleware");
 const roles = require("../middleware/roles.middleware");
 
 routes.post("/sign_Up", signup);
 routes.post("/log_in", login);
+routes.post("/forgotPassword", forgotPassword);
+routes.post("/getverified", verifyMailing);
+routes.get("/verify/:token", userVerification);
+routes.post("/changePassword/:token", changePassword);
+// Authenticated routes
 routes.get("/profile", auth, getMe);
+
 
 module.exports = routes;
